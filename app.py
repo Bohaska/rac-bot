d8c1b033e0e6cde28af79b9ef5d499c36a8a2ed2
Bohaska <73286691+Bohaska@users.noreply.github.com>
2025-03-20 10:10:31 +0800

    add gradio

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import manifold_arkpoint
 from rac_bot import bot
 import nationstates_main
 from ballsdex_hash import hash_balldex_images
+import gradio as gr
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -16,15 +17,23 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
     executor.submit(asyncio.run, autoupdate_grades_habitica(db_client))
     executor.submit(asyncio.run, bot.start(os.getenv('BOT_TOKEN')))"""
 
+def greet(name):
+    return "Hello " + name + "!!"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
 tasks = []
 # tasks.append(Process(target=hash_balldex_images))
 tasks.append(Process(target=bot.run, args=(os.getenv('BOT_TOKEN'),)))
 # tasks.append(Process(target=manifold_arkpoint.bet_stock_markets, args=()))
 tasks.append(Process(target=asyncio.run, args=(nationstates_main.run_all_tasks(),)))
 # bet_users = Process(target=asyncio.run, args=(manifold_arkpoint.bet_users(),))
+tasks.append(Process(target=demo.launch))
+
 for task in tasks:
     task.start()
 # bet_users.start()
 for task in tasks:
     task.join()
 # bet_users.join()
+
